a59d9923f4a705b62d9913d2a8014b8a16b3c6b7
L. Jensen <02Jensenl@github.com>
2020-11-08 22:34:01 -0500

    initial commit

---
+++ TripPlanner.sql
@@ -0,0 +1,55 @@
+--CREATE DATABASE TripPlanner;
+--USE TripPlanner;
+--CREATE TABLE [User](
+--[Name] NVARCHAR(50) PRIMARY KEY NOT NULL,
+--[Address] NVARCHAR (50) NOT NULL,
+--PhoneNumber INT NOT NULL,
+--DOB Date NOT NULL
+--);
+
+--CREATE TABLE Passport(
+--[Name] NVARCHAR(50) NOT NULL,
+--PassportNumber INT NOT NULL,
+--ExpirationDate DATE,
+--PassportAddress NVARCHAR(50)
+--);
+
+--CREATE TABLE Trip(
+--[Name] NVARCHAR(50) NOT NULL,
+--Budget INT,
+--TravelMode NVARCHAR(15),
+--StartDate DATE,
+--EndDate DATE,
+--StartingDestination NVARCHAR(20),
+--EndingDestination NVARCHAR(20)
+--);
+
+--CREATE TABLE Hotel(
+--[Name] NVARCHAR NOT NULL,
+--HotelPhone INT NOT NULL,
+--HotelAddress NVARCHAR(50) NOT NULL,
+--CostPerNight INT NOT NULL,
+--CheckIn DATETIME,
+--CheckOut DATETIME
+--);
+
+--CREATE TABLE PlannedTrip(
+--[Name] NVARCHAR(50) NOT NULL,
+--TripId INT NOT NULL,
+--HotelId INT NOT NULL,
+--NumberOfGuests TINYINT NOT NULL,
+--Notes NVARCHAR(300)
+--);
+
+
+
+
+
+
+
+
+
+
+
+
+
